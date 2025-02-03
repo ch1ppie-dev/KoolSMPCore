05755ac37344e5c3c7686e4593533cfe88a5c3e7
gamingto12 <peanutgallery2005@gmail.com>
2025-02-03 07:43:18 -0500

    Update and fix DoomCommand

--- src/main/java/eu/koolfreedom/command/DoomCommand.java
+++ src/main/java/eu/koolfreedom/command/DoomCommand.java
@@ -1,13 +1,26 @@
 package eu.koolfreedom.command;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.*;
 import org.bukkit.*;
+import org.jetbrains.annotations.NotNull;
 import java.util.*;
 
 @SuppressWarnings("deprecation")
 public class DoomCommand implements CommandExecutor {
-    public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(Component.text("Usage: /" + s + " <player> [reason]", NamedTextColor.RED));
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if (target == null) {
+            sender.sendMessage(Component.text("Could not find the specified player on the server.", NamedTextColor.RED));
+            return true;
+        }
         if (!sender.hasPermission("kf.senior")) {
             sender.sendMessage(ChatColor.RED + "You do not have permission to use this command");
             return false;
