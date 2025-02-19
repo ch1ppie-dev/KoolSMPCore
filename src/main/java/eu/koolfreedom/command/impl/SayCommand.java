fa4c46bdaf773d0b023293210278776155819018
gamingto12 <peanutgallery2005@gmail.com>
2025-02-18 23:12:05 -0500

    i'm basically just rewriting tfm at this point

---
+++ src/main/java/eu/koolfreedom/command/impl/SayCommand.java
@@ -0,0 +1,29 @@
+package eu.koolfreedom.command.impl;
+
+import eu.koolfreedom.KoolSMPCore;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class SayCommand implements CommandExecutor
+{
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if (args.length == 0)
+        {
+            return false;
+        }
+
+        String message = StringUtils.join(args, " ");
+
+        KoolSMPCore.bcastMsg(String.format("[KoolFreedom:%s] %s", sender.getName(), message), ChatColor.LIGHT_PURPLE);
+
+        return true;
+    }
+}
