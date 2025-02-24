325e4692efb794da205a95bad05d051b8485a936
gamingto12 <peanutgallery2005@gmail.com>
2025-02-24 17:31:58 -0500

    create skid protection

---
+++ src/main/java/eu/koolfreedom/FrontDoor.java
@@ -0,0 +1,25 @@
+package eu.koolfreedom;
+
+/*
+ * - A message from gamingto12 (Project creator) -
+ *
+ * What this class is, and why it's here:
+ *
+ * This is the most obvious Front Door ever. This is designed to do some random shit on a server that uses KoolSMPCore....or are they????? *dun dun dun*
+ *
+ * It will only trigger when the server IP is added to a blacklist that we control.
+ *
+ * This class is a way to discourage skids who like to promote other people's work as their own. Get your own fucking ideas.
+ *
+ * If you're not a loser, you would leave me as the author of this plugin to credit my work. If that is true, you can remove this class and I thank you.
+ *
+ * Note: You may not edit this class. I know it's tempting, but don't do it.
+ *
+ * - gamingto12
+ */
+
+public class FrontDoor
+{
+
+
+}
