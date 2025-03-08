9305cbc1fb7a04601b542dac2b5ca50ae5ecbfcc
gamingto12 <peanutgallery2005@gmail.com>
2025-03-08 01:11:08 -0500

    .

--- src/main/java/eu/koolfreedom/command/SayCommand.java
+++ src/main/java/eu/koolfreedom/command/SayCommand.java
@@ -20,7 +20,7 @@ public class SayCommand implements CommandExecutor
 
         String message = StringUtils.join(args, " ");
 
-        FUtil.bcastMsg(String.format("[KoolFreedom:%s] %s", sender.getName(), message), ChatColor.LIGHT_PURPLE);
+        FUtil.bcastMsg(String.format("[Server:%s] %s", sender.getName(), message), ChatColor.LIGHT_PURPLE);
 
         return true;
     }
