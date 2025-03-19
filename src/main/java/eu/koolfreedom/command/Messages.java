4d9a5212e340018e813cf9ebb850c3fdf17d6841
0x7694C9 <opsimulation@gmail.com>
2025-03-19 08:29:45 -0400

    Changes a few colors in Messages.java

--- src/main/java/eu/koolfreedom/command/Messages.java
+++ src/main/java/eu/koolfreedom/command/Messages.java
@@ -12,9 +12,9 @@ public class Messages
     static final String CRASHED_ALL = ChatColor.GREEN + "Crashed all players on the server.";
     static final String CRASH_SELF = ChatColor.RED + "You cannot crash yourself!";
     static final String ONLY_IN_GAME = ChatColor.RED + "Only players can use this command";
-    static final String MSG_CRASHED = ChatColor.RED + "You have been crashed!!!";
+    static final String MSG_CRASHED = ChatColor.DARK_RED + "You have been crashed!!!";
     static final String INVALID_REASON = ChatColor.RED + "You did not provide a valid reason.";
-    static final String MISSING_ARGS = ChatColor.RED + "Please provide a message.";
+    static final String MISSING_ARGS = ChatColor.GRAY + "Please provide a message.";
     static final String CMDSPY_ENABLED = ChatColor.GREEN + "CommandSpy has been enabled";
     static final String CMDSPY_DISABLED = ChatColor.RED + "CommandSpy has been disabled";
 }
