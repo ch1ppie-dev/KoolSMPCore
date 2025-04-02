b58303c9ca758dffd9c475dbe418c8fa91ac27fa
0x7694C9 <opsimulation@gmail.com>
2025-04-02 07:49:28 -0400

    Removes unused imports

--- src/main/java/eu/koolfreedom/listener/PunishmentListener.java
+++ src/main/java/eu/koolfreedom/listener/PunishmentListener.java
@@ -7,7 +7,6 @@ import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import eu.koolfreedom.config.ConfigEntry;
