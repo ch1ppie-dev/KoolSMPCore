0a20d620a5c9c6b75059688d56b1275f07716daa
gamingto12 <peanutgallery2005@gmail.com>
2025-06-18 23:31:54 -0400

    BatChesting pt.2

--- src/main/java/eu/koolfreedom/util/TimeOffset.java
+++ src/main/java/eu/koolfreedom/util/TimeOffset.java
@@ -10,55 +10,46 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Getter
-public enum TimeOffset
-{
-	MILLISECOND(1, "ms"),
-	SECOND(1000, "s"),
-	MINUTE(60 * SECOND.multiplier, "m"),
-	HOUR(60 * MINUTE.multiplier, "h"),
-	DAY(24 * HOUR.multiplier, "d"),
-	WEEK(7 * DAY.multiplier, "w"),
-	MONTH(30 * DAY.multiplier, "d"),
-	YEAR(365 * DAY.multiplier, "y"),
-	CENTURY(10 * YEAR.multiplier, "c"),
-	MILLENNIUM(10 * CENTURY.multiplier, "mil");
+public enum TimeOffset {
+	MILLISECOND(1L, "ms"),
+	SECOND(1000L, "s"),
+	MINUTE(60L * 1000, "m"),
+	HOUR(60L * MINUTE.multiplier, "h"),
+	DAY(24L * HOUR.multiplier, "d"),
+	WEEK(7L * DAY.multiplier, "w"),
+	MONTH(30L * DAY.multiplier, "mo"),
+	YEAR(365L * DAY.multiplier, "y"),
+	CENTURY(100L * YEAR.multiplier, "c"),
+	MILLENNIUM(10L * CENTURY.multiplier, "mil");
 
 	private static final Pattern pattern = Pattern.compile("([0-9]+)([a-z]{1,3})");
 
-	private final int multiplier;
+	private final long multiplier;
 	private final String delimiter;
 
-	TimeOffset(int multiplier, String delimiter)
-	{
+	TimeOffset(long multiplier, String delimiter) {
 		this.multiplier = multiplier;
 		this.delimiter = delimiter;
 	}
 
-	public long multiply(long amount)
-	{
+	public long multiply(long amount) {
 		return amount * multiplier;
 	}
 
-	public static long getOffset(String str)
-	{
+	public static long getOffset(String str) {
 		Matcher matcher = pattern.matcher(str);
-
 		AtomicLong amount = new AtomicLong(0);
-		matcher.results().forEach(result ->
-		{
+
+		matcher.results().forEach(result -> {
 			int amt = Integer.parseInt(result.group(1));
 			String delimiter = result.group(2);
 
 			Optional<TimeOffset> ouch = Arrays.stream(values())
-					.filter(lol -> Objects.nonNull(lol.delimiter))
-					.filter(lol -> lol.delimiter.equalsIgnoreCase(delimiter)).findAny();
+					.filter(offset -> Objects.nonNull(offset.delimiter))
+					.filter(offset -> offset.delimiter.equalsIgnoreCase(delimiter))
+					.findAny();
 
-			if (ouch.isEmpty())
-			{
-				return;
-			}
-
-			amount.set(amount.get() + ouch.get().multiply(amt));
+			ouch.ifPresent(offset -> amount.addAndGet(offset.multiply(amt)));
 		});
 
 		return amount.get();
