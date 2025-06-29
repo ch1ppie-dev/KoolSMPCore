7597b328f32016b95ca00fe1cadb0302048c956d
gamingto12 <peanutgallery2005@gmail.com>
2025-06-29 07:53:47 -0400

    we're gonna pretend i did this the first time

--- src/main/java/eu/koolfreedom/freeze/FreezeListener.java
+++ src/main/java/eu/koolfreedom/freeze/FreezeListener.java
@@ -1,13 +1,20 @@
 package eu.koolfreedom.freeze;
 
 import eu.koolfreedom.KoolSMPCore;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
-public class FreezeListener implements Listener {
+public class FreezeListener implements Listener
+{
+
+    public FreezeListener()
+    {
+        Bukkit.getPluginManager().registerEvents(this, KoolSMPCore.getInstance());
+    }
 
     private final FreezeManager freezeManager = KoolSMPCore.getInstance().getFreezeManager();
 
